0685547843f8180cc2021f79bdb8720d7612deae
Jan Rybarczyk <88709044+jrx-napoli@users.noreply.github.com>
2024-06-27 14:57:06 +0200

    Extended experiments.bat

--- experiments.bat
+++ experiments.bat
@@ -5,6 +5,14 @@ python main.py --y_func f1 --seed 42
 python main.py --y_func f1 --tpa --seed 42
 python main.py --y_func f1 --msr --seed 42
 python main.py --y_func f1 --tpa --msr --seed 42
+python main.py --y_func f1 --seed 13
+python main.py --y_func f1 --tpa --seed 13
+python main.py --y_func f1 --msr --seed 13
+python main.py --y_func f1 --tpa --msr --seed 13
+python main.py --y_func f1 --seed 57
+python main.py --y_func f1 --tpa --seed 57
+python main.py --y_func f1 --msr --seed 57
+python main.py --y_func f1 --tpa --msr --seed 57
 echo finished
 
 echo Executing f2 experiments...
@@ -12,6 +20,14 @@ python main.py --y_func f2 --seed 42
 python main.py --y_func f2 --tpa --seed 42
 python main.py --y_func f2 --msr --seed 42
 python main.py --y_func f2 --tpa --msr --seed 42
+python main.py --y_func f2 --seed 13
+python main.py --y_func f2 --tpa --seed 13
+python main.py --y_func f2 --msr --seed 13
+python main.py --y_func f2 --tpa --msr --seed 13
+python main.py --y_func f2 --seed 57
+python main.py --y_func f2 --tpa --seed 57
+python main.py --y_func f2 --msr --seed 57
+python main.py --y_func f2 --tpa --msr --seed 57
 echo finished
 
 echo Executing f3 experiments...
@@ -19,6 +35,14 @@ python main.py --y_func f3 --seed 42
 python main.py --y_func f3 --tpa --seed 42
 python main.py --y_func f3 --msr --seed 42
 python main.py --y_func f3 --tpa --msr --seed 42
+python main.py --y_func f3 --seed 13
+python main.py --y_func f3 --tpa --seed 13
+python main.py --y_func f3 --msr --seed 13
+python main.py --y_func f3 --tpa --msr --seed 13
+python main.py --y_func f3 --seed 57
+python main.py --y_func f3 --tpa --seed 57
+python main.py --y_func f3 --msr --seed 57
+python main.py --y_func f3 --tpa --msr --seed 57
 echo finished
 
 echo Executing f4 experiments...
@@ -26,6 +50,14 @@ python main.py --y_func f4 --seed 42
 python main.py --y_func f4 --tpa --seed 42
 python main.py --y_func f4 --msr --seed 42
 python main.py --y_func f4 --tpa --msr --seed 42
+python main.py --y_func f4 --seed 13
+python main.py --y_func f4 --tpa --seed 13
+python main.py --y_func f4 --msr --seed 13
+python main.py --y_func f4 --tpa --msr --seed 13
+python main.py --y_func f4 --seed 57
+python main.py --y_func f4 --tpa --seed 57
+python main.py --y_func f4 --msr --seed 57
+python main.py --y_func f4 --tpa --msr --seed 57
 echo finished
 
 echo Executing f5 experiments...
@@ -33,6 +65,14 @@ python main.py --y_func f5 --seed 42
 python main.py --y_func f5 --tpa --seed 42
 python main.py --y_func f5 --msr --seed 42
 python main.py --y_func f5 --tpa --msr --seed 42
+python main.py --y_func f5 --seed 13
+python main.py --y_func f5 --tpa --seed 13
+python main.py --y_func f5 --msr --seed 13
+python main.py --y_func f5 --tpa --msr --seed 13
+python main.py --y_func f5 --seed 57
+python main.py --y_func f5 --tpa --seed 57
+python main.py --y_func f5 --msr --seed 57
+python main.py --y_func f5 --tpa --msr --seed 57
 echo finished
 
 echo Executing f6 experiments...
@@ -40,6 +80,14 @@ python main.py --y_func f6 --seed 42
 python main.py --y_func f6 --tpa --seed 42
 python main.py --y_func f6 --msr --seed 42
 python main.py --y_func f6 --tpa --msr --seed 42
+python main.py --y_func f6 --seed 13
+python main.py --y_func f6 --tpa --seed 13
+python main.py --y_func f6 --msr --seed 13
+python main.py --y_func f6 --tpa --msr --seed 13
+python main.py --y_func f6 --seed 57
+python main.py --y_func f6 --tpa --seed 57
+python main.py --y_func f6 --msr --seed 57
+python main.py --y_func f6 --tpa --msr --seed 57
 echo finished
 
 echo Executing f7 experiments...
@@ -47,6 +95,14 @@ python main.py --y_func f7 --seed 42
 python main.py --y_func f7 --tpa --seed 42
 python main.py --y_func f7 --msr --seed 42
 python main.py --y_func f7 --tpa --msr --seed 42
+python main.py --y_func f7 --seed 13
+python main.py --y_func f7 --tpa --seed 13
+python main.py --y_func f7 --msr --seed 13
+python main.py --y_func f7 --tpa --msr --seed 13
+python main.py --y_func f7 --seed 57
+python main.py --y_func f7 --tpa --seed 57
+python main.py --y_func f7 --msr --seed 57
+python main.py --y_func f7 --tpa --msr --seed 57
 echo finished
 
 echo Executing f8 experiments...
@@ -54,6 +110,14 @@ python main.py --y_func f8 --seed 42
 python main.py --y_func f8 --tpa --seed 42
 python main.py --y_func f8 --msr --seed 42
 python main.py --y_func f8 --tpa --msr --seed 42
+python main.py --y_func f8 --seed 13
+python main.py --y_func f8 --tpa --seed 13
+python main.py --y_func f8 --msr --seed 13
+python main.py --y_func f8 --tpa --msr --seed 13
+python main.py --y_func f8 --seed 57
+python main.py --y_func f8 --tpa --seed 57
+python main.py --y_func f8 --msr --seed 57
+python main.py --y_func f8 --tpa --msr --seed 57
 echo finished
 
 echo Executing f9 experiments...
@@ -61,6 +125,14 @@ python main.py --y_func f9 --seed 42
 python main.py --y_func f9 --tpa --seed 42
 python main.py --y_func f9 --msr --seed 42
 python main.py --y_func f9 --tpa --msr --seed 42
+python main.py --y_func f9 --seed 13
+python main.py --y_func f9 --tpa --seed 13
+python main.py --y_func f9 --msr --seed 13
+python main.py --y_func f9 --tpa --msr --seed 13
+python main.py --y_func f9 --seed 57
+python main.py --y_func f9 --tpa --seed 57
+python main.py --y_func f9 --msr --seed 57
+python main.py --y_func f9 --tpa --msr --seed 57
 echo finished
 
 echo Executing f10 experiments...
@@ -68,5 +140,13 @@ python main.py --y_func f10 --seed 42
 python main.py --y_func f10 --tpa --seed 42
 python main.py --y_func f10 --msr --seed 42
 python main.py --y_func f10 --tpa --msr --seed 42
+python main.py --y_func f10 --seed 13
+python main.py --y_func f10 --tpa --seed 13
+python main.py --y_func f10 --msr --seed 13
+python main.py --y_func f10 --tpa --msr --seed 13
+python main.py --y_func f10 --seed 57
+python main.py --y_func f10 --tpa --seed 57
+python main.py --y_func f10 --msr --seed 57
+python main.py --y_func f10 --tpa --msr --seed 57
 echo finished
 pause
